ab392049a5bfbfb254005b83f8af6e2e4cb684cf
Bryasxin <xinhallow@outlook.com>
2025-08-21 18:03:42 +0800

    feat: 添加团队成员展示页面

---
+++ src/components/member-card.tsx
@@ -0,0 +1,57 @@
+export const Card = (props: ICardProps) => {
+	const { avatarUrl, name, skill, blogWebsite, introduction } = props;
+
+	const pureBlogWebsite = blogWebsite
+		?.replace(/^https?:\/\//, "")
+		.replace(/\/$/, "");
+
+	const blogWebsiteLink = pureBlogWebsite ? (
+		<a
+			href={blogWebsite}
+			target="_blank"
+			rel="noopener noreferrer"
+			className="text-blue-500 hover:text-violet-500"
+		>
+			{pureBlogWebsite}
+		</a>
+	) : (
+		<span className="text-gray-500">No blog website</span>
+	);
+
+	return (
+		<div className="w-full bg-white p-2 rounded-2xl shadow-md">
+			<div className="flex">
+				{/* Avatar */}
+				<img
+					src={avatarUrl}
+					alt={`${name}'s avatar`}
+					className="rounded-2xl size-32 mr-2"
+				/>
+				{/* Name and blog website */}
+				<div className="flex items-center">
+					<div>
+						<div className="text-2xl text-gray-800">{name}</div>
+						<div>{blogWebsiteLink}</div>
+					</div>
+				</div>
+			</div>
+
+			{/* Skills */}
+			<div className="p-1 pl-2 my-2 relative before:absolute before:left-0 before:h-full before:w-1 before:bg-amber-900 dark:before:bg-gray-500">
+				<div className="font-bold mb-1 text-gray-700">专长</div>
+				<div className="text-gray-60">{skill}</div>
+			</div>
+
+			{/* Introduction */}
+			<div>{introduction}</div>
+		</div>
+	);
+};
+
+export interface ICardProps {
+	avatarUrl: string;
+	name: string;
+	skill: string;
+	blogWebsite?: string;
+	introduction: string;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
+import { Card } from "@components/member-card";
+import memberInfos from "@config/members.json";
+
 export const Index = () => {
-	return <></>;
+	const memberCards = memberInfos.map((info) => (
+		<div className="break-inside-avoid mb-4" key={info.name}>
+			<Card {...info} />
+		</div>
+	));
+
+	return (
+		<div className="max-w-[100rem] mx-auto">
+			<div className="text-center text-6xl font-bold my-4 relative before:absolute before:-bottom-2 before:left-[40%] before:w-[20%] before:h-1 before:bg-black">
+				团队成员
+			</div>
+
+			<div className="columns-1 md:columns-2 lg:columns-3 p-4">
+				{memberCards}
+			</div>
+		</div>
+	);
 };
