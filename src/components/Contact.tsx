fcdb28c25ce96fbd1474235b7cc1ab61e69d7a86
Bryasxin <xinhallow@outlook.com>
2025-08-22 20:24:44 +0800

    feat: 添加间距并增加字体大小

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,7 +5,7 @@ export const Contact = () => {
 	return (
 		<div className="contact component">
 			<div className="contact-title">联系我们</div>
-			<Card>
+			<Card className="m-4">
 				<div className="contact-subtitle">欢迎志同道合的朋友加入我们的技术社区</div>
 				<div className="justify-center flex gap-8 flex-wrap p-8">
 					<a href="https://qm.qq.com/q/NeWTRiJRaG" className="contact-item">
